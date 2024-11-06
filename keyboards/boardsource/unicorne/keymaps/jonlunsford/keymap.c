17ec43b18367e99e2e40a797b7f73ffe1d588bb9
Jon Lunsford <jon@liscio.me>
2024-11-05 18:58:24 -0800

    reorder layers

--- keyboards/boardsource/unicorne/keymaps/jonlunsford/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/jonlunsford/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,   KC_T,      KC_Y,  KC_U,   KC_I,    KC_O,   KC_P,      KC_BSPC,
 	HOME_ESC, HOME_A, HOME_S, HOME_D,  HOME_F, KC_G,      KC_H,  HOME_J, HOME_K,  HOME_L, HOME_SCLN, KC_QUOT,
 	KC_LSFT,  KC_Z,   KC_X,   KC_C,    KC_V,   KC_B,      KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH,   RSFT_T(KC_ENT),
-	                          LOWER,   UTIL,   KC_TAB,    KC_ENT, VIMUX, RAISE
+	                          KC_TAB,  LOWER,  UTIL,      VIMUX, RAISE,  KC_ENT
 ),
 
 [_LOWER] = LAYOUT_split_3x6_3(
@@ -75,10 +75,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_UTIL] = LAYOUT_split_3x6_3(
-        _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______,  _______, _______,
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
-    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     _______, _______, _______, _______,  _______, _______,
-	                           _______, _______, _______,    KC_BSPC, _______, _______
+     _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______,  _______, _______,
+    KC_F1,    KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_F6,      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
+    KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     _______, _______, _______, _______,  _______, _______,
+	                           _______, _______, _______,     KC_BSPC, _______, _______
 )
 
 };
