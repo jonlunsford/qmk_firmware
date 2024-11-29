ebbce9e455aad3bea9b34229ff455277abc4f542
Jon Lunsford <jon@liscio.me>
2024-11-28 18:45:43 -0800

    Layer updates

--- keyboards/boardsource/unicorne/keymaps/jonlunsford/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/jonlunsford/keymap.c
@@ -40,10 +40,10 @@ enum unicorne_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_split_3x6_3(
-	KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,   KC_T,      KC_Y,  KC_U,   KC_I,    KC_O,   KC_P,      KC_BSPC,
-	HOME_ESC, HOME_A, HOME_S, HOME_D,  HOME_F, KC_G,      KC_H,  HOME_J, HOME_K,  HOME_L, HOME_SCLN, KC_QUOT,
-	KC_LSFT,  KC_Z,   KC_X,   KC_C,    KC_V,   KC_B,      KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH,   RSFT_T(KC_ENT),
-	                          KC_TAB,  LOWER,  UTIL,      VIMUX, RAISE,  KC_ENT
+	KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,   KC_T,      KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,      KC_BSPC,
+	HOME_ESC, HOME_A, HOME_S, HOME_D,  HOME_F, KC_G,      KC_H,   HOME_J, HOME_K,  HOME_L, HOME_SCLN, KC_QUOT,
+	KC_LSFT,  KC_Z,   KC_X,   KC_C,    KC_V,   KC_B,      KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,   RSFT_T(KC_ENT),
+	                          LOWER,   UTIL,   KC_TAB,    KC_ENT, VIMUX,  RAISE
 ),
 
 [_LOWER] = LAYOUT_split_3x6_3(
